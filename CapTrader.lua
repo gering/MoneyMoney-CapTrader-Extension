ebab1594eef9c50e88782717fc63094608296502
Robert Gering <mail@robertgering.de>
2022-08-01 18:05:58 +0200

    Show cash balances in portfolio, fetch fx rates if rate is missing from cache

--- CapTrader.lua
+++ CapTrader.lua
@@ -13,8 +13,10 @@ WebBanking {
 local token
 local queryId
 local reference
-local cachedContent
-local baseCurrency
+
+-- Cache
+local cachedStatement
+local cachedRates = {}
 
 function SupportsBank(protocol, bankCode)
   return protocol == ProtocolWebBanking and (bankCode == "CapTrader" or bankCode == "IBKR")
@@ -23,16 +25,13 @@ end
 function InitializeSession(protocol, bankCode, username, customer, password)
   queryId = username
   token = password
-  cachedContent = nil
-
-  MM.printStatus("Requesting FlexQuery Reference…")
+  cachedStatement = nil
 
   -- Create HTTPS connection object.
   print("Requesting FlexQuery Reference")
   local url = "https://ndcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest?t=" .. token .. "&q= " .. queryId .. "&v=3"
   local headers = { Accept = "application/xml" }
-  local connection = Connection()
-  local content = connection:request("GET", url, nil, nil, headers)
+  local content = Connection():request("GET", url, nil, nil, headers)
 
   -- Extract status and reference code
   local status = content:match("Status>(.-)<")
@@ -47,46 +46,29 @@ function InitializeSession(protocol, bankCode, username, customer, password)
 end
 
 function ListAccounts(knownAccounts)
-  local statement = getStatement()
-
   -- Parse account info
-  local accountInfo = statement:match("<AccountInformation(.-)/>")
-  local owner = accountInfo:match("name=\"(.-)\"")
-  local accountNumber = accountInfo:match("accountId=\"(.-)\"")
-  local currency = accountInfo:match("currency=\"(.-)\"")
+  local accountInfo = getStatement():match("<AccountInformation(.-)/>")
 
-  local portfolio = {
-    name = "CapTrader " .. MM.localizeText("Portfolio") ,
-    owner = owner,
-    accountNumber = accountNumber,
-    currency = currency,
+  local account = {
+    name = "CapTrader " .. MM.localizeText("Portfolio"),
+    owner = accountInfo:match("name=\"(.-)\""),
+    accountNumber = accountInfo:match("accountId=\"(.-)\""),
+    currency = accountInfo:match("currency=\"(.-)\""),
     portfolio = true,
     type = AccountTypePortfolio
   }
 
-  local account = {
-    name = "CapTrader " .. MM.localizeText("Account"),
-    owner = owner,
-    accountNumber = accountNumber,
-    currency = currency,
-    portfolio = false,
-    type = AccountTypeSavings
-  }
-
-  return {portfolio, account}
+  return {account}
 end
 
 function RefreshAccount(account, since)
-  if account.portfolio == true then
-    return RefreshAccountPortfolio()
-  else
-    return RefreshAccountBalance()
-  end
+  local positions = FetchAccountPositions(account)
+  local balances = FetchAccountBalances(account)
+  return {securities = concat(balances, positions)}
 end
 
-function RefreshAccountPortfolio()
-  local statement = getStatement()
-  local openPositions = statement:match("<OpenPositions(.-)</OpenPositions>")
+function FetchAccountPositions(account)
+  local openPositions = getStatement():match("<OpenPositions(.-)</OpenPositions>")
   local mySecurities = {}
 
   -- parse open positions
@@ -100,59 +82,82 @@ function RefreshAccountPortfolio()
     local costBasisMoney = tonumber(openPosition:match("costBasisMoney=\"(.-)\""))
     local currency = openPosition:match("currency=\"(.-)\"")
     local price = tonumber(openPosition:match("markPrice=\"(.-)\""))
-    local fxRate = tonumber(openPosition:match("fxRateToBase=\"(.-)\""))
+    local fxRateToBase = tonumber(openPosition:match("fxRateToBase=\"(.-)\""))
+    local fxRate = 1 / fxRateToBase
+    cachedRates[currency:lower()] = fxRate
 
     if quantity > 0 then
-      local s = {
+      mySecurities[#mySecurities+1] = {
         name = symbol, -- Bezeichnung des Wertpapiers
         isin = isin, -- ISIN
         market = exchange, -- Börse
         quantity = quantity, -- Nominalbetrag oder Stückzahl
         currency = nil, -- Währung bei Nominalbetrag oder nil bei Stückzahl
-        amount = price * quantity * fxRate, -- Wert der Depotposition in Kontowährung
+        amount = price * quantity * fxRateToBase, -- Wert der Depotposition in Kontowährung
         originalCurrencyAmount = price * quantity, -- Wert der Depotposition in Originalwährung
         currencyOfOriginalAmount = currency, -- Originalwährung
         price = price, -- Aktueller Preis oder Kurs
         currencyOfPrice = currency, -- Von der Kontowährung abweichende Währung des Preises
         purchasePrice = costBasisMoney / quantity, -- Kaufpreis oder Kaufkurs
         currencyOfPurchasePrice = currency, -- Von der Kontowährung abweichende Währung des Kaufpreises
-        exchangeRate = fxRate -- Wechselkurs zum Kaufzeitpunkt
+        -- exchangeRate = fxRate -- Wechselkurs zum Kaufzeitpunkt
       }
-
-      dump(s)
-      mySecurities[#mySecurities+1] = s
     end
   end
 
-  local result = {securities = mySecurities}
-  dump(result)
-  return result
+  return mySecurities
 end
 
--- TODO -> display in balances
-function RefreshAccountBalance()
-  local statement = getStatement()
-  local cashReports = statement:match("<CashReport(.-)</CashReport>")
+function FetchAccountBalances(account)
+  local cashReports = getStatement():match("<CashReport(.-)</CashReport>")
   local myBalances = {}
-  local myBalance = 0
+  local hasForexPositions = false
 
   -- parse cash reports
   for cashReport in cashReports:gmatch("<CashReportCurrency.-/>") do
     print("parsing cash report: " .. cashReport)
 
-    local cash = tonumber(cashReports:match("endingSettledCash=\"(.-)\""))
-    local currency = cashReports:match("currency=\"(.-)\"")
+    local cash = tonumber(cashReport:match("endingSettledCash=\"(.-)\""))
+    local currency = cashReport:match("currency=\"(.-)\"")
 
     if currency == "BASE_SUMMARY" then
-      myBalance = cash
+      myBalances[#myBalances+1] = { 
+        name = MM.localizeText("Settled Cash") .. " (" .. account.currency .. ")",
+        currency = account.currency,
+        quantity = cash
+      }    
     else
-      myBalances[#myBalances+1] = { cash, currency }
+      if currency == account.currency then
+        myBalances[#myBalances+1] = { 
+          name = currency,
+          currency = account.currency,
+          amount = cash
+        }
+      else
+        -- other cash positions, not in base currency
+        hasForexPositions = true
+        local fxRate = getFxRate(account.currency, currency)
+        print(account.currency .. "/" .. currency ..  " = " .. fxRate)
+        local fxRateToBase = 1 / fxRate
+  
+        myBalances[#myBalances+1] = { 
+          name = currency,
+          market =  MM.localizeText("Forex"),
+          quantity = cash, 
+          currency = currency,
+          amount = cash * fxRateToBase
+        }
+      end
     end
   end
 
-  local result = {balance = myBalance, balances = myBalances}
-  dump(result)
-  return result
+  -- If no other currencies are present, add the amout to the base currency
+  if hasForexPositions == false then
+    myBalances[1].amount = myBalances[1].quantity
+    myBalances[1].quantity = nil
+  end
+
+  return myBalances
 end
 
 function EndSession()
@@ -162,14 +167,43 @@ end
 -- Helper
 
 function getStatement()
-  if cachedContent == nil then
-    -- Create HTTPS connection object.
-    print("Requesting FlexQuery Statement")
+  if cachedStatement == nil then
+    print("Fetching FlexQuery Statement")
     local url = "https://ndcdyn.interactivebrokers.com/Universal/servlet/FlexStatementService.GetStatement?t=" .. token .. "&q= " .. reference .. "&v=3"
     local headers = { Accept = "application/xml" }
-    local connection = Connection()
-    cachedContent = connection:request("GET", url, nil, nil, headers)
+    cachedStatement = Connection():request("GET", url, nil, nil, headers)
   end
 
-  return cachedContent
+  return cachedStatement
+end
+
+function concat(t1,t2)
+  for i=1,#t2 do
+      t1[#t1+1] = t2[i]
+  end
+  return t1
+end
+
+function fetchFxRates(base)
+  print("Fetching FX rates: " .. base)
+  local url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/" .. base:lower() .. ".json"
+  local headers = { Accept = "application/json" }
+  local content = Connection():request("GET", url, nil, nil, headers)
+  local json = JSON(content):dictionary()
+  return json[base:lower()]
+end
+
+function getFxRate(base, currency)
+  if base:lower() == currency:lower() then
+    return 1
+  end
+
+  if cachedRates[currency:lower()] ~= nil then
+    return cachedRates[currency:lower()]
+  end
+
+  local rates = fetchFxRates(base)
+  local rate = rates[currency:lower()]
+  cachedRates[currency:lower()] = rate 
+  return rate
 end
